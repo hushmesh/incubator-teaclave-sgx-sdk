a413b77acc7c2feba539d9c577541a36679fb051
Michael Spiegel <michael@hushmesh.com>
2023-06-20 11:31:25 -0400

    Update build.sh and push.sh scripts

--- dockerfile/build.sh
+++ dockerfile/build.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+if [ -z "${CONTAINER_REGISTRY}" ]; then
+	echo "CONTAINER_REGISTRY environment variable not set"
+	exit
+fi
+
 set -e
 
-docker build --no-cache --platform linux/x86_64 -t hushmesh/sgx-rust:ubuntu-2204-sgx-2.19-dcap-1.16 .
+docker build --no-cache --platform linux/x86_64 -t "${CONTAINER_REGISTRY}/sgx-rust:ubuntu-2204-sgx-2.19-dcap-1.16" .
--- dockerfile/push.sh
+++ dockerfile/push.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+if [ -z "${CONTAINER_REGISTRY}" ]; then
+	echo "CONTAINER_REGISTRY environment variable not set"
+	exit
+fi
+
 set -e
 
-docker push --platform linux/x86_64 hushmesh/sgx-rust:ubuntu-2204-sgx-2.19-dcap-1.16
+docker push "${CONTAINER_REGISTRY}/sgx-rust:ubuntu-2204-sgx-2.19-dcap-1.16"
