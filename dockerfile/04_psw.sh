8419b0725c3e95ef48cacaa48045724f6c08dffb
Michael Spiegel <michael@hushmesh.com>
2023-09-26 13:37:30 -0400

    Bugfix add all explicit SGX package versions
    Also install bsdmainutils.

--- dockerfile/04_psw.sh
+++ dockerfile/04_psw.sh
@@ -3,10 +3,25 @@ curl -fsSL -O https://download.01.org/intel-sgx/sgx_repo/ubuntu/intel-sgx-deb.ke
 cat intel-sgx-deb.key | tee /etc/apt/keyrings/intel-sgx-keyring.asc > /dev/null && \
 apt-get update && \
 apt-get install -y \
+	libsgx-ae-pce=$VERSION \
 	libsgx-enclave-common-dev=$VERSION \
+	libsgx-enclave-common=$VERSION \
+	libsgx-headers=$VERSION \
+	libsgx-launch=$VERSION \
+	libsgx-quote-ex=$VERSION \
+	libsgx-urts=$VERSION \
+	sgx-aesm-service=$VERSION \
+	libsgx-ae-id-enclave=$DCAP_VERSION \
+	libsgx-ae-qe3=$DCAP_VERSION \
+	libsgx-ae-qve=$DCAP_VERSION \
 	libsgx-dcap-ql-dev=$DCAP_VERSION \
+	libsgx-dcap-ql=$DCAP_VERSION \
 	libsgx-dcap-default-qpl-dev=$DCAP_VERSION \
-	libsgx-dcap-quote-verify-dev=$DCAP_VERSION && \
+	libsgx-dcap-default-qpl=$DCAP_VERSION \
+	libsgx-dcap-quote-verify-dev=$DCAP_VERSION \
+	libsgx-dcap-quote-verify=$DCAP_VERSION \
+	libsgx-pce-logic=$DCAP_VERSION \
+	libsgx-qe3-logic=$DCAP_VERSION && \
 mkdir /var/run/aesmd && \
 rm -rf /var/lib/apt/lists/* && \
 rm -rf /var/cache/apt/archives/*
