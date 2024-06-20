478d8c1d3a05dc86a28ac43681434e11d56b9b43
Neil Cohen <neil@hushmesh.com>
2024-06-20 22:00:38 +0000

    change docker version

--- dockerfile/07_docker.sh
+++ dockerfile/07_docker.sh
@@ -2,12 +2,7 @@ adduser -u 1005 --disabled-password --home /home/hushmesh --shell /bin/bash --ge
 addgroup --gid 123 docker
 usermod -aG docker hushmesh
 apt-get update
-apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-apt-get update
-apt-get install -y docker-ce docker-ce-cli containerd.io
-usermod -g docker root
+apt-get install -y docker.io
 
 # Install azure cli
 curl -sL https://aka.ms/InstallAzureCLIDeb | bash
