b5bf29963b1599e409af774c45e36acfb928c85f
Neil Cohen <neil@hushmesh.com>
2024-06-20 14:58:09 +0000

    add docker group

--- dockerfile/07_docker.sh
+++ dockerfile/07_docker.sh
@@ -1,6 +1,10 @@
+adduser -u 1005 --disabled-password --home /home/hushmesh --shell /bin/bash --gecos hushmesh hushmesh
+addgroup --gid 123 docker
+usermod -aG docker hushmesh
 apt-get update
 apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update
 apt-get install -y docker-ce docker-ce-cli containerd.io
+usermod -g docker root
